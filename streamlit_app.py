43485275704467643b5354d86bd62d048284b2ea
escherAU <graeme.hay@gmail.com>
2023-02-14 22:15:02 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -87,7 +87,7 @@ def main():
         sorted_anagrams = sorted(anagrams_by_letter.items())
 
         # Print the matching anagrams, grouped by starting letter
-        st.write(f"Matching words ({total_count}):")
+        st.write("Matching words:")
         for letter, (anagrams, count) in sorted(anagrams_by_letter.items()):
             # Add a line break before the anagrams
             st.write(f"\n<h2 style='font-size:24px'>{letter.upper()} ({count}):</h2>", unsafe_allow_html=True)
