135e7228e88307ae15982f4788336aa04b3ad6da
escherAU <graeme.hay@gmail.com>
2023-06-06 11:44:29 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,7 +59,6 @@ def main():
     common_pool = common_pool.replace(" ", "")
 
     if all(c.isalpha() for c in common_pool) and len(common_pool) == 7:
-        # Get a set of all English words
         all_words = set(
             word.upper()
             for word in pd.read_csv(
@@ -71,38 +70,29 @@ def main():
             ).values.flatten()
         )
 
-        # Call the get_anagrams function to get the matching anagrams
         matching_anagrams = get_anagrams(common_pool, all_words)
 
-        # Ask the user to enter a letter to filter the anagrams
         letter = st.text_input("Enter the center letter to filter the list:", key="filter_letter")
         letter = letter.upper()
         if letter:
-            # Filter the anagrams by the specified letter
             matching_anagrams = filter_by_letter(matching_anagrams, letter)
 
-        # Filter the anagrams by valid English words
         matching_anagrams = filter_by_valid_words(matching_anagrams, all_words)
 
-        # Create a dictionary to group the anagrams by starting letter
         anagrams_by_letter = defaultdict(lambda: ([], 0))
-            
         for anagram, is_pangram in matching_anagrams:
-        key = anagram[0]
-        anagrams, count = anagrams_by_letter[key]
-        anagrams.append((anagram, is_pangram))
-        anagrams_by_letter[key] = (anagrams, count + 1)
+            key = anagram[0]
+            anagrams, count = anagrams_by_letter[key]
+            anagrams.append((anagram, is_pangram))
+            anagrams_by_letter[key] = (anagrams, count + 1)
 
-        # Sort the anagrams by starting letter and store them in a list of tuples
         sorted_anagrams = sorted(anagrams_by_letter.items())
-        # Print the matching anagrams, grouped by starting letter
+
         st.write("Matching words:")
         for letter, (anagrams, count) in sorted_anagrams:
-            # Add a line break before the anagrams
             st.write(f"\n<h2 style='font-size:24px'>{letter.upper()} ({count}):</h2>", unsafe_allow_html=True)
-            # Wrap the anagrams so that they don't fall within the scrollbars
             with st.container():
-                anagrams.sort(key=lambda x: x[0])  # sort by the word part of the tuple
+                anagrams.sort(key=lambda x: x[0])
                 anagrams_to_write = []
                 for word, is_pangram in anagrams:
                     if is_pangram:
